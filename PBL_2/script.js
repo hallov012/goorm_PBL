f95fff9d58e0bd63aa357d11243e5451bceb2a5e
hyelim <hallov012@gmail.com>
2023-08-27 16:09:43 +0900

    feat: PBL2

---
+++ PBL_2/script.js
@@ -0,0 +1,140 @@
+const addBtn = document.querySelector(".add-btn");
+const todoList = document.querySelector(".todo-list");
+
+/* todo 추가 */
+const addTodo = (todo, idx) => {
+  const todoWrap = document.createElement("li");
+  todoWrap.classList.add("todo-wrap");
+  todoWrap.id = idx;
+
+  /* left 영역 */
+  const todoLeft = document.createElement("div");
+  todoLeft.classList.add("todo-left");
+
+  const todoCheck = document.createElement("input");
+  todoCheck.type = "checkbox";
+  todoCheck.checked = todo.checked;
+  todoCheck.classList.add("todo-check");
+  todoCheck.id = `todo-check-${idx}`;
+
+  const todoCheckLabel = document.createElement("label");
+  todoCheckLabel.classList.add("todo-check-label");
+  todoCheckLabel.htmlFor = `todo-check-${idx}`;
+
+  const todoText = document.createElement("span");
+  todoText.classList.add("todo-text");
+  console.log(todo.text);
+  todoText.innerText = todo.text;
+
+  todoLeft.appendChild(todoCheck);
+  todoLeft.appendChild(todoCheckLabel);
+  todoLeft.appendChild(todoText);
+
+  /* right 영역 */
+  const todoRight = document.createElement("div");
+  todoRight.classList.add("todo-right");
+
+  const todoEdit = document.createElement("i");
+  todoEdit.classList = "fa-solid fa-pencil edit-btn";
+
+  const todoDelete = document.createElement("i");
+  todoDelete.classList = "fa-solid fa-circle-minus remove-btn";
+
+  todoRight.appendChild(todoEdit);
+  todoRight.appendChild(todoDelete);
+
+  /* todoWrap에 left, right 영역 추가 */
+  todoWrap.appendChild(todoLeft);
+  todoWrap.appendChild(todoRight);
+
+  /* todoList에 todoWrap 추가 */
+  todoList.appendChild(todoWrap);
+};
+
+/* todoList에 todo 추가 */
+const renderTodos = () => {
+  todoList.innerHTML = "";
+  todos.forEach((todo, idx) => {
+    addTodo(todo, idx);
+  });
+};
+
+/* ㅣocalStorage에 todos 저장 */
+const saveTodo = () => {
+  localStorage.setItem("todos", JSON.stringify(todos));
+};
+
+const savedTodo = localStorage.getItem("todos");
+const todos = saveTodo ? JSON.parse(savedTodo) : [];
+renderTodos();
+
+/* 새로운 todo 추가 */
+addBtn.addEventListener("click", () => {
+  const newData = {
+    text: "새로운 할 일",
+    checked: false,
+  };
+
+  todos.push(newData);
+  saveTodo();
+  renderTodos();
+});
+
+const removeTodo = (idx) => {
+  todos.splice(idx, 1);
+  saveTodo();
+  renderTodos();
+};
+
+const editBtnHandler = (target) => {
+  const todoTextArea = target.parentNode.parentNode.childNodes[0].childNodes[2];
+  const todoText = todoTextArea.innerHTML;
+  const editBtn = target.parentNode.childNodes[0];
+
+  /* input으로 변경 */
+  todoTextArea.innerHTML = "";
+
+  const input = document.createElement("input");
+  input.type = "text";
+  input.value = todoText;
+  input.classList.add("todo-input");
+
+  todoTextArea.appendChild(input);
+
+  /* editBtn 변경 */
+  editBtn.classList = "fa-solid fa-check edit-save-btn";
+};
+
+const editSaveHandler = (idx, target) => {
+  const todoText = target.parentNode.parentNode.childNodes[0].childNodes[2];
+  const editBtn = target.parentNode.childNodes[0];
+
+  /* 변경내용 저장 */
+  todos[idx].text = todoText.childNodes[0].value;
+  saveTodo();
+  renderTodos();
+
+  /* editBtn 변경 */
+  editBtn.classList = "fa-solid fa-pencil edit-btn";
+};
+
+const todoCheck = (idx, target) => {
+  todos[idx].checked = target.checked;
+  saveTodo();
+  renderTodos();
+};
+
+/* todo 수정 or 삭제 */
+todoList.addEventListener("click", (e) => {
+  const target = e.target;
+  const idx = target.parentNode.parentNode.id;
+  if (target.classList.contains("remove-btn")) {
+    removeTodo(idx);
+  } else if (target.classList.contains("edit-btn")) {
+    editBtnHandler(target);
+  } else if (target.classList.contains("edit-save-btn")) {
+    editSaveHandler(idx, target);
+  } else if (target.classList.contains("todo-check")) {
+    todoCheck(idx, target);
+  }
+});
--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # goorm_PBL
 
 ### 1. (0818-0822) HTML, CSS를 이용해서 넷플릭스 사이트 만들기
+
 [결과물 링크](https://64df2144fba5f51b739d511e--magical-souffle-35d57c.netlify.app/)
+
+### 2. (0824-0828) 자바스크립트를 이용해서 Todo 앱 만들기
+
+[결과물 링크]()
